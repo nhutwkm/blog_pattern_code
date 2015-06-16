970923cf740d8f810c5facd6d65144c1daf8c1b4
chuhongson-pc <hongsonbk1@gmail.com>
2015-06-16 16:34:50 +0700

    3_CreateCommands

--- db/migrate/20150616070211_create_commands.rb
+++ db/migrate/20150616070211_create_commands.rb
@@ -5,7 +5,7 @@ class CreateCommands < ActiveRecord::Migration
       t.string :description
       t.interger :id_user
 
-      t.timestamps null: false
+      t.timestamps null: true
     end
   end
 end
