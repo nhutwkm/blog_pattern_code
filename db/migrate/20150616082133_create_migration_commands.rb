2a1c521c4658db8219f198fbc60f09002dec63ed
chuong2v <chuong.2v@gmail.com>
2015-06-16 16:14:54 +0700

    create model commands and categories_posts

---
+++ db/migrate/20150616082133_create_migration_commands.rb
@@ -0,0 +1,9 @@
+class CreateMigrationCommands < ActiveRecord::Migration
+  def change
+    create_table :commands do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.text :comment
+    end
+  end
+end
