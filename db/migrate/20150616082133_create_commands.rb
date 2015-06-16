7d2674b8effca0f13e1bbbaf71299af6d79d682a
chuong2v <chuong.2v@gmail.com>
2015-06-16 15:47:40 +0700

    fix loi

--- db/migrate/20150616082133_create_commands.rb
+++ db/migrate/20150616082133_create_commands.rb
@@ -1,8 +1,8 @@
 class CreateCommands < ActiveRecord::Migration
   def change
     create_table :commands do |t|
-      t.int :user_id
-      t.int :post_id
+      t.integer :user_id
+      t.integer :post_id
       t.text :comment
     end
   end
