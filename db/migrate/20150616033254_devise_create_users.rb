0ac0b0d87de31e0acd1191914038323ac9b61cd5
YOUR NAME <YOUR@EMAIL.com>
2015-06-16 14:23:56 +0700

    Revert "file role trong db"
    This reverts commit a645ecf6c0a073091c05761b42bc906e6e016f77.

--- db/migrate/20150616033254_devise_create_users.rb
+++ db/migrate/20150616033254_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.integer :id ,               null: false, default: 0
       t.string :name,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/migrate/20150616065330_role.rb
+++
@@ -1,9 +0,0 @@
-class Role < ActiveRecord::Migration
-  def change
-  	create_table :Role do |t|
-      t.string :name
-      t.text :dese
- 
-      t.timestamps null: false
-  end
-end
