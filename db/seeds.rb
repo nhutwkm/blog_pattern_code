7b479a2145fbf98527fdfcffa9bdda33157d102f
Duy Dy <duydyy@gmail.com>
2015-06-17 13:47:37 +0700

    Tao seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# roles_list = [["admin", "admin"], ["user", "user"]]
+roles_list = [["admin", "admin"], ["user", "user"]]
 
-# roles_list.each do |name, desc|
-#   Role.create!(name: name, desc: desc)
-# end
+roles_list.each do |name, desc|
+  Role.create!(name: name, desc: desc)
+end
 
 user_list = [
   ["chuong",    "chuong@gmail.com", "user"],
@@ -32,41 +32,41 @@ user_list.each do |user, email, role|
   				role: Role.find_by_name(role)
           )
 end
-# category_list = ["Ruby", "HTML", "PHP", "CSS", "JavaScript", "Java"]
+category_list = ["Ruby", "HTML", "PHP", "CSS", "JavaScript", "Java"]
 
-# category_list.each do |category|
-#   Category.create(name: category)
-# end
-# post_list = [
-#   ["Hello voi Ruby",    "Viet chunog trinh dau tien voi Ruby",  "puts 'Hello world'", "1"],
-#   ["Hello voi Java",    "Viet chunog trinh dau tien voi Java",  "System.out.println('Hello world')", "2"]
-# ]
-# post_list.each do |title, desc, code, userid|
-#   Post.create(title: title,
-#               desc: desc,
-#               code: code,
-#               user_id: userid)
-# end
-# categories_posts_list = [
-#   ["1", "1"],
-#   ["2", "6"]
-# ]
-# categories_posts_list.each do |postid, cateid|
-#   CategoriesPost.create(
-#             post_id: postid,
-#             category_id: cateid
-#     )
-# end
-# command_list = [
-#   ["4", "1",  "Khong the tin noi"],
-#   ["5", "1",  "That tuyet voi"],
-#   ["4", "2",  "Khong the tin noi"],
-#   ["5", "2",  "That tuyet voi"]
-# ]
-# command_list.each do |userid, postid, comment|
-#   Command.create(
-#           user_id: userid,
-#           post_id: postid,
-#           comment: comment
-#     )
-# end
+category_list.each do |category|
+  Category.create(name: category)
+end
+post_list = [
+  ["Hello voi Ruby",    "Viet chunog trinh dau tien voi Ruby",  "puts 'Hello world'", "1"],
+  ["Hello voi Java",    "Viet chunog trinh dau tien voi Java",  "System.out.println('Hello world')", "2"]
+]
+post_list.each do |title, desc, code, userid|
+  Post.create(title: title,
+              desc: desc,
+              code: code,
+              user_id: userid)
+end
+categories_posts_list = [
+  ["1", "1"],
+  ["2", "6"]
+]
+categories_posts_list.each do |postid, cateid|
+  CategoriesPost.create(
+            post_id: postid,
+            category_id: cateid
+    )
+end
+command_list = [
+  ["4", "1",  "Khong the tin noi"],
+  ["5", "1",  "That tuyet voi"],
+  ["4", "2",  "Khong the tin noi"],
+  ["5", "2",  "That tuyet voi"]
+]
+command_list.each do |userid, postid, comment|
+  Command.create(
+          user_id: userid,
+          post_id: postid,
+          comment: comment
+    )
+end
