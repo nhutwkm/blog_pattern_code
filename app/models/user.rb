eff51b4d4dd4e696cf3a57faeee778715a492d3e
YOUR NAME <YOUR@EMAIL.com>
2015-06-17 08:41:37 +0700

    new quan he

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-      belongs_to :post
-      has_many :role
+      has_many :posts
+      has_many :comments
+      belongs_to :role
 end
