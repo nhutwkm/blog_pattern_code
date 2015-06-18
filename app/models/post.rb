ed3c4cdd20ca0a9469d4df04375bbc0b487a4592
tienbian <tientranvu@gmail.com>
2015-06-18 09:27:31 +0700

    them CRUD POST

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,19 @@
 class Post < ActiveRecord::Base
 	has_many :commands
 	has_many :categories_posts
-	has_many :categories, through :categories_posts
+	has_many :categories, through: :categories_posts
 	belongs_to :user 
 
+	def self.do_list
+      Post.all
+  	end
+	def self.do_new(post_params)
+	  Post.new(post_params)
+	end
+	def self.delete(id)
+	  Post.find(id).destroy
+	end	
+	def self.post_list(page)
+      Post.paginate(:page =>page, :per_page => 3).order('title asc')	 
+	end
 end
