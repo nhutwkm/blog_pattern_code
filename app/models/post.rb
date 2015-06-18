c7e1e4b166baa14923ae028972f78dd810922d47
chuong2v <chuong.2v@gmail.com>
2015-06-18 08:30:06 +0700

    liet ke danh sach cac bai post cua 1 user

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Base
 	has_many :categories, through: :categories_posts
 	belongs_to :user 
 
+	def self.list_posts_of_user(user)
+		Post.where(user_id: user.id)
+	end
+
 end
