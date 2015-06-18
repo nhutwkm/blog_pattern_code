41b48cedff0bac5938ae423523010b79b9019fa8
tienbian <tientranvu@gmail.com>
2015-06-18 11:09:19 +0700

    crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+		def index
+    	  @posts = Post.do_list
+      	  @posts = Post.post_list(params[:page])
+  		end
+		
+		def new
+		  @users = User.all
+		  @post = Post.new
+		end
+
+		def create
+		  @post = Post.do_new(post_params)
+		  if @post.save
+		    redirect_to @post
+			else
+			render 'new'
+			end 
+		end
+
+		def update
+		  @post = Post.find(params[:id])
+		  if @post.update(post_params)
+		    redirect_to posts_path
+			else 
+			  render 'edit'
+			end
+		end
+
+		def edit 
+		  @users = User.all
+		  @post = Post.find(params[:id])
+		end
+		
+		def show	
+    	  @post = Post.find(params[:id])
+  		end
+  		def destroy
+	      @post = Post.delete(params[:id])
+		  @post.destroy
+    	  redirect_to posts_path
+  		end
+  	private
+  		def post_params
+    	  params.require(:post).permit(:title, :desc, :code, :user_id)
+  		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'users#index'
+  resources :posts do
+      resources :commands
+    end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
