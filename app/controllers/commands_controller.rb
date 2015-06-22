25da418dcf451e9449ae62654aefcb6e0557115e
tienbian <tientranvu@gmail.com>
2015-06-22 11:14:02 +0700

    delete cm

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -4,6 +4,13 @@ class CommandsController < ApplicationController
     @comment = @post.commands.create(user_id: current_user.id, comment: comment_params[:comment])
     redirect_to post_path(@post)
   end
+  def destroy
+  @post = Post.find(params[:post_id])
+  	@comment = Command.find(params[:id])
+  	@comment.destroy
+  	redirect_to @post
+end
+
  
   private
     def comment_params
